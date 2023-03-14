944980237aa435287b355287507e1082683ca054
Juan Daniel Halomoan <47490009+donjuan136@users.noreply.github.com>
2023-03-14 22:45:40 +0700

    first commit

---
+++ dbt-assigment/chicago_taxi_trips/target/compiled/chicago_taxi_trips/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `datafellowship-juan`.`chicago_taxi_trips`.`my_first_dbt_model`
+where id is null
+
+
---
+++ dbt-assigment/chicago_taxi_trips/target/compiled/chicago_taxi_trips/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `datafellowship-juan`.`chicago_taxi_trips`.`my_second_dbt_model`
+where id is null
+
+
